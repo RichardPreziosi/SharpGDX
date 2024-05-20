b5761a4c6186e98fd31286f698b3dda50caf6a56
Richard Preziosi <17318983+RichardPreziosi@users.noreply.github.com>
2024-05-20 11:53:06 -0400

    Somewhat ported some Net stuff.

--- src/SharpGDX.Desktop/DesktopNet.cs
+++ src/SharpGDX.Desktop/DesktopNet.cs
@@ -9,5 +9,34 @@ namespace SharpGDX.Desktop
 	public class Lwjgl3Net : Net
 	{
 		public Lwjgl3Net(Lwjgl3ApplicationConfiguration config) { }
+		public void sendHttpRequest(Net.HttpRequest httpRequest, Net.HttpResponseListener? httpResponseListener)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void cancelHttpRequest(Net.HttpRequest httpRequest)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ServerSocket newServerSocket(Net.Protocol protocol, string hostname, int port, ServerSocketHints hints)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ServerSocket newServerSocket(Net.Protocol protocol, int port, ServerSocketHints hints)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Socket newClientSocket(Net.Protocol protocol, string host, int port, SocketHints hints)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool openURI(string URI)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
--- src/SharpGDX.Headless/HeadlessNet.cs
+++ src/SharpGDX.Headless/HeadlessNet.cs
@@ -3,4 +3,33 @@
 public class HeadlessNet : Net
 {
 	public HeadlessNet(HeadlessApplicationConfiguration configuration){}
+	public void sendHttpRequest(Net.HttpRequest httpRequest, Net.HttpResponseListener? httpResponseListener)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void cancelHttpRequest(Net.HttpRequest httpRequest)
+	{
+		throw new NotImplementedException();
+	}
+
+	public ServerSocket newServerSocket(Net.Protocol protocol, string hostname, int port, ServerSocketHints hints)
+	{
+		throw new NotImplementedException();
+	}
+
+	public ServerSocket newServerSocket(Net.Protocol protocol, int port, ServerSocketHints hints)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Socket newClientSocket(Net.Protocol protocol, string host, int port, SocketHints hints)
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool openURI(string URI)
+	{
+		throw new NotImplementedException();
+	}
 }
