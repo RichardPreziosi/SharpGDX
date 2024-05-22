660cc3dab589c705c310201e44d5573bd28b40b7
Richard Preziosi <17318983+RichardPreziosi@users.noreply.github.com>
2024-05-22 03:32:47 -0400

    Initial port of Maps namespace.

--- src/SharpGDX/Assets/Loaders/BitmapFontLoader.cs
+++ src/SharpGDX/Assets/Loaders/BitmapFontLoader.cs
@@ -1,37 +1,49 @@
-//using SharpGDX.Shims;
-//using SharpGDX.Utils;
-//using SharpGDX.Mathematics;
+using SharpGDX.Shims;
+using static SharpGDX.Graphics.G2D.TextureAtlas;
+using static SharpGDX.Graphics.Texture;
+using SharpGDX.Utils;
+using SharpGDX.Graphics;
+using SharpGDX.Graphics.G2D;
+using SharpGDX.Graphics.GLUtils;
+using static SharpGDX.Graphics.G2D.BitmapFont;
+using SharpGDX.Mathematics;
 
-//namespace SharpGDX.Assets.Loaders;
+namespace SharpGDX.Assets.Loaders;
 
-///** {@link AssetLoader} for {@link BitmapFont} instances. Loads the font description file (.fnt) asynchronously, loads the
-// * {@link Texture} containing the glyphs as a dependency. The {@link BitmapFontParameter} allows you to set things like texture
-// * filters or whether to flip the glyphs vertically.
-// * @author mzechner */
-//public class BitmapFontLoader : AsynchronousAssetLoader<BitmapFont, BitmapFontLoader.BitmapFontParameter> {
-//	public BitmapFontLoader (FileHandleResolver resolver) {
-//		super(resolver);
-//	}
+/** {@link AssetLoader} for {@link BitmapFont} instances. Loads the font description file (.fnt) asynchronously, loads the
+ * {@link Texture} containing the glyphs as a dependency. The {@link BitmapFontParameter} allows you to set things like texture
+ * filters or whether to flip the glyphs vertically.
+ * @author mzechner */
+public class BitmapFontLoader : AsynchronousAssetLoader<BitmapFont, BitmapFontLoader.BitmapFontParameter>
+{
+	public BitmapFontLoader(FileHandleResolver resolver)
+	: base(resolver)
+	{
+		
+	}
 
-//	BitmapFontData data;
+	BitmapFontData data;
 
-//	@Override
-//	public Array<AssetDescriptor> getDependencies (String fileName, FileHandle file, BitmapFontParameter parameter) {
-//		Array<AssetDescriptor> deps = new Array();
-//		if (parameter != null && parameter.bitmapFontData != null) {
+	public override Array<AssetDescriptor<BitmapFont>> getDependencies(String fileName, FileHandle file, BitmapFontParameter parameter)
+	{
+		throw new NotImplementedException();
+//		Array<AssetDescriptor> deps = new();
+//		if (parameter != null && parameter.bitmapFontData != null)
+//		{
 //			data = parameter.bitmapFontData;
 //			return deps;
 //		}
 
 //		data = new BitmapFontData(file, parameter != null && parameter.flip);
-//		if (parameter != null && parameter.atlasName != null) {
-//			deps.add(new AssetDescriptor(parameter.atlasName, TextureAtlas.class));
+//		if (parameter != null && parameter.atlasName != null)
+//		{
+//			deps.add(new AssetDescriptor<TextureAtlas>(parameter.atlasName, typeof(TextureAtlas)));
 //		} else {
-//			for (int i = 0; i < data.getImagePaths().length; i++) {
+//			for (int i = 0; i<data.getImagePaths().Length; i++) {
 //				String path = data.getImagePath(i);
-//				FileHandle resolved = resolve(path);
+//FileHandle resolved = resolve(path);
 
-//				TextureLoader.TextureParameter textureParams = new TextureLoader.TextureParameter();
+//TextureLoader.TextureParameter textureParams = new TextureLoader.TextureParameter();
 
 //				if (parameter != null) {
 //					textureParams.genMipMaps = parameter.genMipMaps;
@@ -39,60 +51,63 @@
 //					textureParams.magFilter = parameter.magFilter;
 //				}
 
-//				AssetDescriptor descriptor = new AssetDescriptor(resolved, Texture.class, textureParams);
-//				deps.add(descriptor);
+//				AssetDescriptor descriptor = new AssetDescriptor<Texture>(resolved, typeof(Texture), textureParams);
+//deps.add(descriptor);
 //			}
 //		}
 
 //		return deps;
-//	}
+	}
 
-//	@Override
-//	public void loadAsync (AssetManager manager, String fileName, FileHandle file, BitmapFontParameter parameter) {
-//	}
+	public override void loadAsync(AssetManager manager, String fileName, FileHandle file, BitmapFontParameter parameter)
+{
+}
 
-//	@Override
-//	public BitmapFont loadSync (AssetManager manager, String fileName, FileHandle file, BitmapFontParameter parameter) {
-//		if (parameter != null && parameter.atlasName != null) {
-//			TextureAtlas atlas = manager.get(parameter.atlasName, TextureAtlas.class);
-//			String name = file.sibling(data.imagePaths[0]).nameWithoutExtension().toString();
-//			AtlasRegion region = atlas.findRegion(name);
+public override BitmapFont loadSync(AssetManager manager, String fileName, FileHandle file, BitmapFontParameter parameter)
+{
+	if (parameter != null && parameter.atlasName != null)
+	{
+		TextureAtlas atlas = manager.get< TextureAtlas>(parameter.atlasName, typeof(TextureAtlas));
+String name = file.sibling(data.imagePaths[0]).nameWithoutExtension().ToString();
+AtlasRegion region = atlas.findRegion(name);
 
-//			if (region == null)
-//				throw new GdxRuntimeException("Could not find font region " + name + " in atlas " + parameter.atlasName);
-//			return new BitmapFont(file, region);
-//		} else {
-//			int n = data.getImagePaths().length;
-//			Array<TextureRegion> regs = new Array(n);
-//			for (int i = 0; i < n; i++) {
-//				regs.add(new TextureRegion(manager.get(data.getImagePath(i), Texture.class)));
-//			}
-//			return new BitmapFont(data, regs, true);
-//		}
-//	}
+if (region == null)
+	throw new GdxRuntimeException("Could not find font region " + name + " in atlas " + parameter.atlasName);
+return new BitmapFont(file, region);
+		} else
+{
+	int n = data.getImagePaths().Length;
+	Array<TextureRegion> regs = new (n);
+	for (int i = 0; i < n; i++)
+	{
+		regs.add(new TextureRegion(manager.get< Texture>(data.getImagePath(i), typeof(Texture))));
+			}
+			return new BitmapFont(data, regs, true);
+		}
+	}
 
-//	/** Parameter to be passed to {@link AssetManager#load(String, Class, AssetLoaderParameters)} if additional configuration is
-//	 * necessary for the {@link BitmapFont}.
-//	 * @author mzechner */
-//	static public class BitmapFontParameter extends AssetLoaderParameters<BitmapFont> {
-//		/** Flips the font vertically if {@code true}. Defaults to {@code false}. **/
-//		public boolean flip = false;
+	/** Parameter to be passed to {@link AssetManager#load(String, Class, AssetLoaderParameters)} if additional configuration is
+	 * necessary for the {@link BitmapFont}.
+	 * @author mzechner */
+	 public class BitmapFontParameter : AssetLoaderParameters<BitmapFont> {
+		/** Flips the font vertically if {@code true}. Defaults to {@code false}. **/
+		public bool flip = false;
 
-//		/** Generates mipmaps for the font if {@code true}. Defaults to {@code false}. **/
-//		public boolean genMipMaps = false;
+/** Generates mipmaps for the font if {@code true}. Defaults to {@code false}. **/
+public bool genMipMaps = false;
 
-//		/** The {@link TextureFilter} to use when scaling down the {@link BitmapFont}. Defaults to {@link TextureFilter#Nearest}. */
-//		public TextureFilter minFilter = TextureFilter.Nearest;
+/** The {@link TextureFilter} to use when scaling down the {@link BitmapFont}. Defaults to {@link TextureFilter#Nearest}. */
+public TextureFilter minFilter = TextureFilter.Nearest;
 
-//		/** The {@link TextureFilter} to use when scaling up the {@link BitmapFont}. Defaults to {@link TextureFilter#Nearest}. */
-//		public TextureFilter magFilter = TextureFilter.Nearest;
+/** The {@link TextureFilter} to use when scaling up the {@link BitmapFont}. Defaults to {@link TextureFilter#Nearest}. */
+public TextureFilter magFilter = TextureFilter.Nearest;
 
-//		/** optional {@link BitmapFontData} to be used instead of loading the {@link Texture} directly. Use this if your font is
-//		 * embedded in a {@link Skin}. **/
-//		public BitmapFontData bitmapFontData = null;
+/** optional {@link BitmapFontData} to be used instead of loading the {@link Texture} directly. Use this if your font is
+ * embedded in a {@link Skin}. **/
+public BitmapFontData bitmapFontData = null;
 
-//		/** The name of the {@link TextureAtlas} to load the {@link BitmapFont} itself from. Optional; if {@code null}, will look
-//		 * for a separate image */
-//		public String atlasName = null;
-//	}
-//}
+/** The name of the {@link TextureAtlas} to load the {@link BitmapFont} itself from. Optional; if {@code null}, will look
+ * for a separate image */
+public String atlasName = null;
+	}
+}
--- src/SharpGDX/Assets/Loaders/CubemapLoader.cs
+++ src/SharpGDX/Assets/Loaders/CubemapLoader.cs
@@ -1,81 +1,96 @@
-//using SharpGDX.Shims;
-//using SharpGDX.Utils;
-//using SharpGDX.Mathematics;
+using SharpGDX.Shims;
+using SharpGDX.Utils;
+using SharpGDX.Mathematics;
+using SharpGDX.Graphics.GLUtils;
+using SharpGDX.Graphics;
 
-//namespace SharpGDX.Assets.Loaders;
+namespace SharpGDX.Assets.Loaders;
 
-///** {@link AssetLoader} for {@link Cubemap} instances. The pixel data is loaded asynchronously. The texture is then created on the
-// * rendering thread, synchronously. Passing a {@link CubemapParameter} to
-// * {@link AssetManager#load(String, Class, AssetLoaderParameters)} allows one to specify parameters as can be passed to the
-// * various Cubemap constructors, e.g. filtering and so on.
-// * @author mzechner, Vincent Bousquet */
-//public class CubemapLoader : AsynchronousAssetLoader<Cubemap, CubemapLoader.CubemapParameter> {
-//	static public class CubemapLoaderInfo {
-//		String filename;
-//		CubemapData data;
-//		Cubemap cubemap;
-//	};
+/** {@link AssetLoader} for {@link Cubemap} instances. The pixel data is loaded asynchronously. The texture is then created on the
+ * rendering thread, synchronously. Passing a {@link CubemapParameter} to
+ * {@link AssetManager#load(String, Class, AssetLoaderParameters)} allows one to specify parameters as can be passed to the
+ * various Cubemap constructors, e.g. filtering and so on.
+ * @author mzechner, Vincent Bousquet */
+public class CubemapLoader : AsynchronousAssetLoader<Cubemap, CubemapLoader.CubemapParameter>
+{
+	public class CubemapLoaderInfo
+	{
+		internal String filename;
+		internal CubemapData data;
+		internal Cubemap cubemap;
+	};
 
-//	CubemapLoaderInfo info = new CubemapLoaderInfo();
+	CubemapLoaderInfo info = new CubemapLoaderInfo();
 
-//	public CubemapLoader (FileHandleResolver resolver) {
-//		super(resolver);
-//	}
+	public CubemapLoader(FileHandleResolver resolver)
+	: base(resolver)
+	{
+		
+	}
 
-//	@Override
-//	public void loadAsync (AssetManager manager, String fileName, FileHandle file, CubemapParameter parameter) {
-//		info.filename = fileName;
-//		if (parameter == null || parameter.cubemapData == null) {
-//			Format format = null;
-//			boolean genMipMaps = false;
-//			info.cubemap = null;
+	public override void loadAsync(AssetManager manager, String fileName, FileHandle file, CubemapParameter parameter)
+	{
+		info.filename = fileName;
+		if (parameter == null || parameter.cubemapData == null)
+		{
+			Pixmap.Format? format = null;
+			bool genMipMaps = false;
+			info.cubemap = null;
 
-//			if (parameter != null) {
-//				format = parameter.format;
-//				info.cubemap = parameter.cubemap;
-//			}
+			if (parameter != null)
+			{
+				format = parameter.format;
+				info.cubemap = parameter.cubemap;
+			}
 
-//			if (fileName.contains(".ktx") || fileName.contains(".zktx")) {
-//				info.data = new KTXTextureData(file, genMipMaps);
-//			}
-//		} else {
-//			info.data = parameter.cubemapData;
-//			info.cubemap = parameter.cubemap;
-//		}
-//		if (!info.data.isPrepared()) info.data.prepare();
-//	}
+			if (fileName.Contains(".ktx") || fileName.Contains(".zktx"))
+			{
+				info.data = new KTXTextureData(file, genMipMaps);
+			}
+		}
+		else
+		{
+			info.data = parameter.cubemapData;
+			info.cubemap = parameter.cubemap;
+		}
+		if (!info.data.isPrepared()) info.data.prepare();
+	}
 
-//	@Override
-//	public Cubemap loadSync (AssetManager manager, String fileName, FileHandle file, CubemapParameter parameter) {
-//		if (info == null) return null;
-//		Cubemap cubemap = info.cubemap;
-//		if (cubemap != null) {
-//			cubemap.load(info.data);
-//		} else {
-//			cubemap = new Cubemap(info.data);
-//		}
-//		if (parameter != null) {
-//			cubemap.setFilter(parameter.minFilter, parameter.magFilter);
-//			cubemap.setWrap(parameter.wrapU, parameter.wrapV);
-//		}
-//		return cubemap;
-//	}
+	public override Cubemap loadSync(AssetManager manager, String fileName, FileHandle file, CubemapParameter parameter)
+	{
+		if (info == null) return null;
+		Cubemap cubemap = info.cubemap;
+		if (cubemap != null)
+		{
+			cubemap.load(info.data);
+		}
+		else
+		{
+			cubemap = new Cubemap(info.data);
+		}
+		if (parameter != null)
+		{
+			cubemap.setFilter(parameter.minFilter, parameter.magFilter);
+			cubemap.setWrap(parameter.wrapU, parameter.wrapV);
+		}
+		return cubemap;
+	}
 
-//	@Override
-//	public Array<AssetDescriptor> getDependencies (String fileName, FileHandle file, CubemapParameter parameter) {
-//		return null;
-//	}
+	public override Array<AssetDescriptor<Cubemap>> getDependencies(String fileName, FileHandle file, CubemapParameter parameter)
+	{
+		return null;
+	}
 
-//	static public class CubemapParameter extends AssetLoaderParameters<Cubemap> {
-//		/** the format of the final Texture. Uses the source images format if null **/
-//		public Format format = null;
-//		/** The texture to put the {@link TextureData} in, optional. **/
-//		public Cubemap cubemap = null;
-//		/** CubemapData for textures created on the fly, optional. When set, all format and genMipMaps are ignored */
-//		public CubemapData cubemapData = null;
-//		public TextureFilter minFilter = TextureFilter.Nearest;
-//		public TextureFilter magFilter = TextureFilter.Nearest;
-//		public TextureWrap wrapU = TextureWrap.ClampToEdge;
-//		public TextureWrap wrapV = TextureWrap.ClampToEdge;
-//	}
-//}
+	public class CubemapParameter : AssetLoaderParameters<Cubemap> {
+		/** the format of the final Texture. Uses the source images format if null **/
+		public Pixmap.Format? format = null;
+	/** The texture to put the {@link TextureData} in, optional. **/
+	public Cubemap cubemap = null;
+	/** CubemapData for textures created on the fly, optional. When set, all format and genMipMaps are ignored */
+	public CubemapData cubemapData = null;
+	public Texture.TextureFilter minFilter = Texture.TextureFilter.Nearest;
+	public Texture.TextureFilter magFilter = Texture.TextureFilter.Nearest;
+	public Texture.TextureWrap wrapU = Texture.TextureWrap.ClampToEdge;
+	public Texture.TextureWrap wrapV = Texture.TextureWrap.ClampToEdge;
+}
+}
